b5c31a9c4c8fd26fd639b4d20667a98bf0f18f15
Aliona21 <142835385+Aliona21@users.noreply.github.com>
2023-08-21 23:09:30 +0300

    Update pagrindinis.py

--- pagrindinis.py
+++ pagrindinis.py
@@ -1,9 +1,13 @@
+# Importuojame reikiamas bibliotekas
 import tkinter
 from tkinter import messagebox
 import generatorius
 import logging
 
+# Konfigūruojame pasirinktą lygį žurnaliavimui į failą
 logging.basicConfig(filename='slaptazodziai.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+# Funkcija, kuri generuoja slaptažodžius pagal nurodytą kiekį ir ilgį
 def generuoti_slaptazodzius():
     try:
         slaptazodziu_kiekis = int(kiekis.get())
@@ -13,6 +17,7 @@ def generuoti_slaptazodzius():
 
     try:
         slaptazodzio_ilgis = int(ilgis.get())
+        # Tikriname, ar slaptažodžio ilgis yra tarp 8 ir 127 simbolių
         if slaptazodzio_ilgis < 8:
             messagebox.showwarning("Klaida", "Įvestas slaptažodžio ilgis per trumpas")
             return
@@ -30,16 +35,19 @@ def generuoti_slaptazodzius():
         nauji_slaptazodziai.append(slaptazodis)
         logging.info('Naujas slaptazodis pridetas i faila')
 
+    # Įrašome naujus slaptažodžius į failą
     with open('slaptazodziai.txt', 'a') as failas:
         failas.write('\n'.join(nauji_slaptazodziai) + '\n')
 
     sugeneruoti = "\n".join(nauji_slaptazodziai)
+    # Sukuriame naują langą su sugeneruotais slaptažodžiais
     slaptazodziai_langas = tkinter.Toplevel(langas)
     slaptazodziai_langas.title("Sugeneruoti slaptažodžiai")
     tkinter.Label(slaptazodziai_langas, text="Jūsų sugeneruoti slaptažodžiai:").pack()
     tkinter.Label(slaptazodziai_langas, text=sugeneruoti).pack()
     tkinter.Label(slaptazodziai_langas, text="Slaptažodžiai pridėti į failą 'slaptazodziai.txt'.").pack()
 
+# Funkcija, kuri rodo esamus slaptažodžius iš failo
 def rodyti_slaptazodzius():
     try:
         with open('slaptazodziai.txt', 'r') as failas:
@@ -50,15 +58,17 @@ def rodyti_slaptazodzius():
     except FileNotFoundError:
         messagebox.showerror("Klaida", "Failas slaptazodziai.txt nerastas")
 
+# Funkcija, kuri išeina iš programos
 def iseiti():
     if messagebox.askyesno("Išeiti", "Ar tikrai norite išeiti?"):
         langas.destroy()
 
+# Sukuriame pagrindinį langą
 langas = tkinter.Tk()
 langas.title("Slaptažodžių Generatorius")
-
 langas.geometry("320x200")
 
+# Sveikinimo ir nustatymų įvedimo laukai bei mygtukai
 pasveikinimas = tkinter.Label(langas, text="Sveiki. Ši programa generuoja sunkius slaptažodžius\nir įrašo juos į failą slaptazodziai.txt")
 pasveikinimas.pack()
 
@@ -79,4 +89,5 @@ mygtukas_sarasas.pack()
 mygtukas_iseiti = tkinter.Button(langas, text="Išeiti", command=iseiti)
 mygtukas_iseiti.pack()
 
+# Paleidžiame pagrindinį GUI (grafinį vartotojo sąsajos) langą
 langas.mainloop()
