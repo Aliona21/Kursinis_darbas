c2e7898da0c3d3a96da245969456fe164bf94c59
Aliona21 <142835385+Aliona21@users.noreply.github.com>
2023-08-21 23:02:15 +0300

    Update generatorius.py

--- generatorius.py
+++ generatorius.py
@@ -1,10 +1,13 @@
+# Importuojame reikiamas bibliotekas
 import string
 import random
 
+# Funkcija, kuri generuoja sąrašą specialių simbolių ir tikrina, ar slaptažodis atitinka reikalavimus
 def slaptazodziu_generatorius(ilgis):
     simboliai = string.ascii_letters + string.digits + string.punctuation
     while True:
         slaptazodis = ''.join(random.choice(simboliai) for _ in range(ilgis))
+        # Tikriname, ar slaptažodyje yra bent vienas skaičius, mažoji raidė, didžioji raidė ir specialus simbolis
         if (any(c.isdigit() for c in slaptazodis) and
             any(c.islower() for c in slaptazodis) and
             any(c.isupper() for c in slaptazodis) and
